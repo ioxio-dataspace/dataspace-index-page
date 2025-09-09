94623750c44e20ec62325cf8585fb240514fee65
fbjorn <denis@fbjorn.cc>
2025-09-09 14:00:24 +0300

    Add dataspace name to config

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -1,6 +1,7 @@
 export default {
   COMPONENT_NAME: "{{ .Any.DIP_COMPONENT_NAME }}",
   DATASPACE_URL: "{{ .Any.DIP_DATASPACE_URL }}",
+  DATASPACE_NAME: "{{ .Any.DIP_DATASPACE_NAME }}",
   DEV_PORTAL_URL: "{{ .Any.DIP_DEV_PORTAL_URL }}",
   OG_IMAGE: "{{ .Any.DIP_OG_IMAGE }}",
   IOXIO_URL: "https://ioxio.com/dataspace",
